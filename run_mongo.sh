b3193a525f6a0accb31e1799a755562ff75d3923
Anjali Kumari Singh <94233830+anjali29singh@users.noreply.github.com>
2023-04-08 09:53:48 +0530

    Delete run_mongo.sh

--- run_mongo.sh
+++
@@ -1,8 +0,0 @@
-echo "Running mongo db on port 27017"
-
-docker run --rm  -d -p 27017:27017 \
-    -e MONGO_INITDB_ROOT_USERNAME=admin \
-    -e MONGO_INITDB_ROOT_PASSWORD=admin \
-    --mount type=bind,source="$(pwd)"/mongo-db-urls,target=/data/db  \
-     --network=shrinky-app \
-    mongo:latest
